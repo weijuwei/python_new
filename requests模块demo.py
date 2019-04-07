007d849a8a2afc950506ef64ed61a94d849bb062
weijuwei <583112952@qq.com>
2019-04-07 18:51:50 +0800

    v2

--- requests模块demo.py
+++ requests模块demo.py
@@ -1,6 +1,5 @@
 import urllib
 import requests
-from bs4 import BeautifulSoup
 
 ##定制请求头部
 headers = {
@@ -15,35 +14,12 @@ html.encoding = 'gbk' #设置编码格式
 status_code = html.status_code#检测响应状态码 如果是4xx 5xx的状态码会抛出异常
 print(status_code)
 
+data = html.text #取得响应的内容的文本形式
+#print(data)
+
+content = html.content##二进制响应内容
+#print(content)
+
 res_header = html.headers #查看一个以字典形式展示的服务器响应头
 print(res_header)
 print(res_header['Content-Type'])
-'''
-{'Date': 'Sun, 07 Apr 2019 09:34:46 GMT',
- 'Content-Type': 'text/html', 
- 'Transfer-Encoding': 'chunked', 
- 'Connection': 'keep-alive', 
- 'Vary': 'Accept-Encoding', 
- 'P-State': 'ESOLC', 
- 'Last-Modified': 'Sat, 06 Apr 2019 23:48:37 GMT', 
- 'ETag': 'W/"5ca93ad5-4ee0"', 
- 'Server': 'HiCDN', 
- 'Expires': 'Mon, 08 Apr 2019 09:34:46 GMT', 
- 'Cache-Control': 'max-age=86400', 
- 'X-Cache-Status': 'HIT', 
- 'XPage': '1d', 
- 'Content-Encoding': 'gzip'}
-'''
-
-
-data = html.text #取得响应的内容
-#print(data)
-
-'''
-soup = BeautifulSoup(data,'html.parser')
-img_class = soup.select('.big-pic') 
-img_label = img_class[0]
-img_src = img_label.find('img')
-print(img_src['src'])
-'''
-
