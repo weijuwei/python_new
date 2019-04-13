0ad5b7710ad69a5ea59c342d5ad2e3a5c7f8352d
weijuwei <583112952@qq.com>
2019-04-13 21:52:04 +0800

    modify some files

---
+++ 类方法属性.py
@@ -0,0 +1,29 @@
+# 类对象可以直接调用类属性，也可以直接调用类方法
+# 但不允许调用实例属性，并且也不允许调用实例方法
+# 实例对象可以获取实例属性和类属性的值，调用实例方法和类方法，但不能修改类属性
+
+class Students:
+	__stu_nums = 0 # 类属性 私有  此处用作统计实例化的数量
+
+	def __init__(self,name,age):  # 实例方法 
+		self.name = name  # 实例属性
+		self.age = age
+		self.set_stu_nums()  # 每实例化一个对象 调用类方法
+
+	def __str__(self):
+		return self.name+' '+ str(self.age)
+
+	@classmethod  # 类方法 @classmethod 装饰器
+	def set_stu_nums(cls): # 类方法
+		cls.__stu_nums += 1
+
+	def get_stu_nums(self):
+		return self.__stu_nums
+
+stu1 = Students("Tom".ljust(6," "),23)  # ljust()左对齐
+print(stu1)
+stu2 = Students("Jerry".ljust(6," "),25)
+print(stu2)
+stu3 = Students("Bob".ljust(6," "),28)
+print(stu3)
+print(stu1.get_stu_nums())
