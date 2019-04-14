ac3931832f6ad9b35795ae846d835a591e6a530c
weijuwei <583112952@qq.com>
2019-04-14 17:46:39 +0800

    8人随机分配办公室v2

--- 随机分配位置demo.py
+++ 随机分配位置demo.py
@@ -11,14 +11,14 @@ for room in office_rooms:
         tea = teachers.pop(index)  # 将机选出的人从teachers中删除，并将结果赋值给tea
         room.append(tea)
         
-for tea in teachers:  # 将teachers中的人随机放到office当中
+for teacher in teachers:  # 将teachers中剩下的人随机放到office当中
     index = random.randint(0,2)
-    office_rooms[index].append(tea)
+    office_rooms[index].append(teacher)
 
 i = 1        
 for room in office_rooms:  # 打印出分配结果
 	print("办公室%d:" %i,end='\t')
-	for tea in room:
-		print(tea,end=" ")
+	for teacher in room:
+		print(teacher,end=" ")
 	print()	
 	i += 1
