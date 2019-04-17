997f99ab4cc88dc78f90b38a06258b116e46ef3e
weijuwei <583112952@qq.com>
2019-04-18 00:59:03 +0800

    v2

--- dev/excel/excel_99乘法表.py
+++ dev/excel/excel_99乘法表.py
@@ -12,14 +12,10 @@ for i in range(2,11):
 	ws.cell(row=i,column=1).value = i - 1
 
 for i in range(2,11):
-	# r = ws.cell(row=1,column=i).value
-	# c = ws.cell(row=1,column=i).value
-	# ws.cell(row=i,column=i).value = r * c
-	# print(r,c)
 	for j in range(2,11):
 		row = ws.cell(row=i,column=1).value
 		column = ws.cell(row=1,column=j).value
 		ws.cell(row=i,column=j).value = row * column
 
-if not os.path.isfile('99乘法表.xlsx')
+if not os.path.isfile('99乘法表.xlsx'):
 	wb.save('99乘法表.xlsx')
