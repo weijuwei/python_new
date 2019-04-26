9d25dbf2f3d3d3febdeb56eb50ea38f55cc0d6dc
weijuwei <583112952@qq.com>
2019-04-26 13:01:30 +0800

    classmethod v2

--- class/类方法属性.py
+++ class/类方法属性.py
@@ -3,7 +3,7 @@
 # 实例对象可以获取实例属性和类属性的值，调用实例方法和类方法，但不能修改类属性
 
 class Students:
-	__stu_nums = 0 # 类属性 私有  此处用作统计实例化的数量
+	__stu_nums = 0 # 类属性 伪私有  此处用作统计实例化的数量
 
 	def __init__(self,name,age):  # 实例方法 
 		self.name = name  # 实例属性
@@ -20,6 +20,13 @@ class Students:
 	def get_stu_nums(self):
 		return self.__stu_nums
 
+	@classmethod
+	def get_stu_instance(cls,stu_str):  # 通过字符串,返回一个对象实例
+		name,age = stu_str.split('-')
+		age = int(age)
+		return cls(name,age)
+
+
 stu1 = Students("Tom".ljust(6," "),23)  # ljust()左对齐
 print(stu1)
 stu2 = Students("Jerry".ljust(6," "),25)
@@ -27,3 +34,6 @@ print(stu2)
 stu3 = Students("Bob".ljust(6," "),28)
 print(stu3)
 print(stu1.get_stu_nums())
+stu_str = 'Lucy-22'
+stu4 = Students.get_stu_instance(stu_str)
+print(stu4)
